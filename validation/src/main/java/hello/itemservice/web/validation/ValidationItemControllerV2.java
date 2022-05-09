0a1d1460d423d7d1643e3c294e6b59aecd9332f4
who-hoo <who.ho3ov@gmail.com>
2022-05-09 11:47:59 +0900

    :zap: 오류가 발생하는 경우 고객이 입력한 내용이 모두 사라지는 문제 해결
    FieldError의 rejectedValue를 사용하여 오류 발생 시 사용자 입력 값을 별도로 보관, 화면에 다시 출력

--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -48,7 +48,7 @@ public class ValidationItemControllerV2 {
         return "validation/v2/addForm";
     }
 
-    @PostMapping("/add")
+    //    @PostMapping("/add")
     public String addItemV1(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
 
         // 검증 로직(특정 필드)
@@ -67,6 +67,49 @@ public class ValidationItemControllerV2 {
             int resultPrice = item.getPrice() * item.getQuantity();
             if (resultPrice < 10000) {
                 bindingResult.addError(new ObjectError("item", "수량은 최대 9,999까지 허용합니다."));
+            }
+        }
+
+        // 검증에 실패하면 다시 입력 폼으로
+        if (bindingResult.hasErrors()) {
+            log.info("errors={}", bindingResult);
+            return "validation/v2/addForm";
+        }
+
+        // 성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute Item item, BindingResult bindingResult,
+        RedirectAttributes redirectAttributes, Model model) {
+
+        // 검증 로직(특정 필드)
+        if (!StringUtils.hasText(item.getItemName())) {
+            bindingResult.addError(
+                new FieldError("item", "itemName", item.getItemName(), false, null, null,
+                    "상품 이름은 필수입니다."));
+        }
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+            bindingResult.addError(
+                new FieldError("item", "price", item.getPrice(), false, null, null,
+                    "가격은 1,000 ~ 1,000,000까지 허용합니다."));
+        }
+        if (item.getQuantity() == null || item.getQuantity() >= 9999) {
+            bindingResult.addError(
+                new FieldError("item", "quantity", item.getQuantity(), false, null, null,
+                    "수량은 최대 9,999까지 허용합니다."));
+        }
+
+        // 검증 로직(특정 필드가 아닌 복합 룰)
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                bindingResult.addError(
+                    new ObjectError("item", null, null, "수량은 최대 9,999까지 허용합니다."));
             }
         }
 

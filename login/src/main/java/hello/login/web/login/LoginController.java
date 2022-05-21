882a7482a5f132714032c41dffa922190242e8b4
who-hoo <who.ho3ov@gmail.com>
2022-05-22 03:30:18 +0900

    :sparkles: 쿠키를 사용한 로그인 처리 구현

--- login/src/main/java/hello/login/web/login/LoginController.java
+++ login/src/main/java/hello/login/web/login/LoginController.java
@@ -3,6 +3,8 @@ package hello.login.web.login;
 import hello.login.domain.login.LoginService;
 import hello.login.domain.member.Member;
 import hello.login.web.login.form.LoginForm;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -25,7 +27,11 @@ public class LoginController {
 	}
 
 	@PostMapping("/login")
-	public String login(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult) {
+	public String login(
+		@Valid @ModelAttribute LoginForm form,
+		BindingResult bindingResult,
+		HttpServletResponse response
+	) {
 		if (bindingResult.hasErrors()) {
 			return "login/loginForm";
 		}
@@ -38,8 +44,21 @@ public class LoginController {
 			return "login/loginForm";
 		}
 
-		// TODO : 로그인 성공 처리
+		Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
+		response.addCookie(idCookie);
 
 		return "redirect:/";
+	}
+
+	@PostMapping("logout")
+	public String logout(HttpServletResponse response) {
+		expireCookie(response, "memberId");
+		return "redirect:/";
+	}
+
+	private void expireCookie(HttpServletResponse response, String cookieName) {
+		Cookie cookie = new Cookie(cookieName, null);
+		cookie.setMaxAge(0);
+		response.addCookie(cookie);
 	}
 }

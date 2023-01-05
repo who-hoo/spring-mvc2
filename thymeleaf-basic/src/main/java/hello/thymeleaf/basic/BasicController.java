2a02964c16f78132934f533718f72b85181f3ac8
who-hoo <who.ho3ov@gmail.com>
2023-01-05 23:45:21 +0900

    ✨ 연산 학습

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -68,6 +68,13 @@ public class BasicController {
         return "basic/literal";
     }
 
+    @GetMapping("/operation")
+    public String operation(Model model) {
+        model.addAttribute("nullData", null);
+        model.addAttribute("data", "spring!");
+        return "basic/operation";
+    }
+
     @Data
     static class User {
 
